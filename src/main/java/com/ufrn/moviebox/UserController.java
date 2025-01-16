6e9e7a4bf27a033c35c22a573bc6e2e5b192b903
flawbert <contato.flawbertlorran@hotmail.com>
2025-01-16 18:43:43 -0300

    Adding ListView and Buttons at User screen

--- src/main/java/com/ufrn/moviebox/UserController.java
+++ src/main/java/com/ufrn/moviebox/UserController.java
@@ -4,6 +4,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import models.Movie;
+import models.Session;
+import models.User;
 
 public class UserController {
 
@@ -11,16 +14,53 @@ public class UserController {
     private Button buttonVoltarTelaMovie;
 
     @FXML
-    private ListView<?> listViewFilmes;
+    private Button buttonMostrarDados;
 
     @FXML
-    private ListView<?> listViewDados;
+    private Button buttonMostrarFilmes;
+
+    @FXML
+    private ListView<String> listViewFilmes;
+
+    @FXML
+    private ListView<String> listViewDados;
 
     @FXML
     void clickButtonVoltarTelaMovie(ActionEvent event) {
         System.out.println("voltar tela movie");
         Main.changeScreen("movie");
-
     }
 
+    @FXML
+    void clickMostrarDados(ActionEvent event) {
+        listViewDados.getItems().clear();
+        User user = Session.getLoggedUser();
+        if (user != null) {
+            listViewDados.getItems().add("Nome: " + user.getName());
+            listViewDados.getItems().add("CPF: " + user.getCpf());
+            listViewDados.getItems().add("Email: " + user.getEmail());
+        } else {
+            listViewDados.getItems().add("ERRO! Não há usuário logado.");
+        }
+    }
+
+    @FXML
+    void clickMostrarFilmes(ActionEvent event) {
+        listViewFilmes.getItems().clear();
+
+        User user = Session.getLoggedUser();
+
+        if (user != null) {
+            if (user.getRentedMovies().isEmpty()) {
+                listViewFilmes.getItems().add("Nenhum filme alugado.");
+            } else {
+                for (Movie movie : user.getRentedMovies()) {
+                    listViewFilmes.getItems().add("Título: " + movie.getTitle() + " | Aluguel: " + movie.getRentalDays() + " dias");
+                }
+            }
+        } else {
+            listViewFilmes.getItems().add("ERRO! Não há usuário logado.");
+        }
+    }
 }
+
