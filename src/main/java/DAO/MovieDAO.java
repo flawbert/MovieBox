cf8e49217453ca4d4071f0cf210dbdbe6eb71f93
flawbert <contato.flawbertlorran@hotmail.com>
2025-01-16 20:38:48 -0300

    Switching movies genres and enum

--- src/main/java/DAO/MovieDAO.java
+++ src/main/java/DAO/MovieDAO.java
@@ -10,15 +10,15 @@ public class MovieDAO implements Serializable {
     private static final List<Movie> movies = new ArrayList<>();
 
     static {
-        movies.add(new Movie("Jogos Vorazes", Movie_genre.COMEDIA, 1));
-        movies.add(new Movie("Interestelar", Movie_genre.COMEDIA, 2));
+        movies.add(new Movie("Jogos Vorazes", Movie_genre.DRAMA, 1));
+        movies.add(new Movie("Interestelar", Movie_genre.DRAMA, 2));
         movies.add(new Movie("Wicked", Movie_genre.COMEDIA, 3));
-        movies.add(new Movie("Lagoa Azul", Movie_genre.COMEDIA, 4));
-        movies.add(new Movie("Gladiador", Movie_genre.COMEDIA, 5));
+        movies.add(new Movie("Lagoa Azul", Movie_genre.ROMANCE, 4));
+        movies.add(new Movie("Gladiador", Movie_genre.ACAO, 5));
         movies.add(new Movie("Carros", Movie_genre.COMEDIA, 6));
-        movies.add(new Movie("Logan", Movie_genre.COMEDIA, 7));
-        movies.add(new Movie("Furiosa", Movie_genre.COMEDIA, 8));
-        movies.add(new Movie("The substance", Movie_genre.COMEDIA,9));
+        movies.add(new Movie("Logan", Movie_genre.DRAMA, 7));
+        movies.add(new Movie("Furiosa", Movie_genre.ACAO, 8));
+        movies.add(new Movie("The substance", Movie_genre.TERROR,9));
         movies.add(new Movie("Mulan", Movie_genre.COMEDIA,10));
     }
 
